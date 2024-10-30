c562fbaebfde9453b74aa6d2949cd007530887cf
Isroilov Azizbek <aziz16110904@gmail.com>
2024-10-30 09:11:23 +0500

    full working video upload

--- app/Http/Controllers/VimeoController.php
+++ app/Http/Controllers/VimeoController.php
@@ -7,15 +7,18 @@ use Vimeo\Laravel\Facades\Vimeo;
 
 class VimeoController extends Controller
 {
+    public function __construct()
+    {
+
+    }
     public function upload(Request $request)
     {
-        // dd($request->video);
         $url = Vimeo::upload($request->video, [
             'name' => 'new title',
             'description' => 'new description',
         ]);
         return response()->json([
-            'url' => $url,
+            'url' => Vimeo::request($url),
         ]);
     }
 }
